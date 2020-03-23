b590ba9d9f7a8930aaf4e62c278ce98dd6611a03
Kornelone99 <jankoradovic84@gmail.com>
2020-03-23 19:38:11 +0100

    Add high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (lat,lon,callback) => {
 			callback('Unable to find location',undefined)
 		}
 		else{
-			callback(undefined,`${body.daily.data[0].summary} It is currently ${body.currently.temperature}. There is ${body.currently.precipProbability}% chance of rain`)
+			callback(undefined,`${body.daily.data[0].summary} It is currently ${body.currently.temperature}.This high today is ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow}. There is ${body.currently.precipProbability}% chance of rain`)
 		}
 
 	})
